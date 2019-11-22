286e99319ed0928f917d881fc6b17dca8052a9d7
TheIcyOne <TheIcyOne@users.noreply.github.com>
2019-11-22 23:36:13 +0000

    this is a git update
    yay

--- src/main/java/com/headfishindustries/claybutithaseveryoredictinthegame/claybutithaseveryoredictinthegame.java
+++ src/main/java/com/headfishindustries/claybutithaseveryoredictinthegame/claybutithaseveryoredictinthegame.java
@@ -2,6 +2,7 @@ package com.headfishindustries.claybutithaseveryoredictinthegame;
 
 import net.minecraft.init.Items;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.oredict.OreDictionary;
@@ -9,7 +10,7 @@ import net.minecraftforge.oredict.OreDictionary;
 @Mod(modid="claybutithaseveryoredictinthegame", name="claybutithaseveryoredictinthegame", version="1.0.0")
 public class claybutithaseveryoredictinthegame {
 
-	@SubscribeEvent
+	@EventHandler
 	public static void postInit(FMLPostInitializationEvent e) {
 		for (String s : OreDictionary.getOreNames()) {
 			OreDictionary.registerOre(s, Items.CLAY_BALL);
